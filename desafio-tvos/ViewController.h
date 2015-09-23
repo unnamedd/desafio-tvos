3da4f209b01abe930118dc680c88f78296cb25ef
Thiago Holanda <thiago.holanda@concretesolutions.com.br>
2015-09-23 16:19:12 -0300

    initial commit

---
+++ desafio-tvos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  desafio-tvos
+//
+//  Created by Thiago Holanda on 9/23/15.
+//  Copyright © 2015 Concrete Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ desafio-tvos/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  desafio-tvos
+//
+//  Created by Thiago Holanda on 9/23/15.
+//  Copyright © 2015 Concrete Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
