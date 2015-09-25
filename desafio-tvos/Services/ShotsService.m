7bd733537f64d83b566a804ba5c858f0e02e99ad
Thiago Holanda <thiago.holanda@concretesolutions.com.br>
2015-09-25 01:09:59 -0300

    implementado a classe de serviço para buscar os shots do dribble

--- desafio-tvos/Services/ShotsService.m
+++ desafio-tvos/Services/ShotsService.m
@@ -6,8 +6,68 @@
 //  Copyright © 2015 Concrete Solutions. All rights reserved.
 //
 
-#import "ShotsService.h"
+#import "Services.h"
+
+@interface ShotsService()
+
+@property (strong, nonatomic) BaseService *baseService;
+
+@end
 
 @implementation ShotsService
 
++ (instancetype) sharedInstance {
+    static ShotsService *shareClient;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        shareClient = [[self alloc] init];
+    });
+    
+    return shareClient;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self)
+        self.baseService = [BaseService sharedInstance];
+    
+    return self;
+}
+
+- (void) get:(ShotModel *) shot complete:(ServiceResultShotBlock) complete {
+    NSString *URL = [NSString stringWithFormat:@"/shots/%@", shot.uuid];
+    
+    [self.baseService GET: URL
+               parameters: nil
+                 complete: ^(NSURLResponse *response, id responseObject, NSError *error) {
+                     
+                     ShotModel *shot;
+                     
+                     if (!error && [responseObject isKindOfClass: NSDictionary.class]) {
+                         shot = [[ShotModel alloc] initWithDictionary: responseObject
+                                                                error: &error];
+                     }
+                     
+                     if (complete)
+                         complete(shot, error);
+                 }];
+}
+
+- (void) listPopularWithPage:(NSInteger) page complete:(ServiceResultListBlock) complete {
+    NSString *URL = @"/shots/popular";
+    
+    NSDictionary *params = @{
+                             @"per_page": @(10),
+                             @"page": @(page)
+                             };
+    
+    [self.baseService GET: URL
+               parameters: params
+                 complete:^(NSURLResponse *response, id responseObject, NSError *error) {
+                     NSLog(@"Object: %@", responseObject);
+                 }];
+}
+
 @end
+
