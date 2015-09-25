b1682bef0e0fd17fb3885e8a38bdf266f93abd3d
Thiago Holanda <thiago.holanda@concretesolutions.com.br>
2015-09-25 01:09:34 -0300

    implementado uma simples estrutura básica para as requisições HTTP
    é necessário refatorar essa classe

--- desafio-tvos/Services/Services.h
+++ desafio-tvos/Services/Services.h
@@ -8,6 +8,37 @@
 
 #import <Foundation/Foundation.h>
 
-@interface Services : NSObject
+// Models
+#import "Models.h"
+
+// Generic
+typedef void (^BaseServiceResponseBlock)(NSURLResponse *response, id responseObject, NSError *error);
+
+typedef void (^ServiceResultListBlock)(NSArray *response, NSInteger totalPages, NSError *error);
+typedef void (^ServiceResultObjectBlock)(id object, BOOL success, NSError *error);
+
+// Shots
+typedef void (^ServiceResultShotBlock)(ShotModel *shot, NSError *error);
+
+#pragma mark - Base Service
+@interface BaseService : NSObject
+
++ (instancetype) sharedInstance;
+
+#pragma mark - Request Methods
+- (void) GET: (NSString *) URLString
+  parameters: (NSDictionary *) parameters
+    complete: (BaseServiceResponseBlock) complete;
+
+@end
+
+@interface ShotsService : NSObject
+
+#pragma mark - Initializers
++ (instancetype) sharedInstance;
+
+#pragma mark - Request Methods
+- (void) get:(ShotModel *) shot complete:(ServiceResultShotBlock) complete;
+- (void) listPopularWithPage:(NSInteger) page complete:(ServiceResultListBlock) complete;
 
 @end
--- desafio-tvos/Services/Services.m
+++ desafio-tvos/Services/Services.m
@@ -7,7 +7,128 @@
 //
 
 #import "Services.h"
+#import "CommonDefines.h"
 
-@implementation Services
+
+@interface BaseService()
+
+@property (strong, nonatomic) NSURL *baseURL;
+@property (strong, nonatomic) NSURLSession *baseSession;
+
+@end
+
+@implementation BaseService
+
+#pragma mark - Shared Instance
+
++ (instancetype) sharedInstance {
+    static BaseService *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    
+    return sharedInstance;
+}
+
+#pragma mark - Util
+
+#pragma mark - Initialization
+
+- (instancetype) init {
+    self = [super init];
+    
+    if (self) {
+        self.baseURL = [NSURL URLWithString: URL_BASE];
+        
+        NSURLSessionConfiguration *sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+        self.baseSession = [NSURLSession sessionWithConfiguration: sessionConfig
+                                                         delegate: nil
+                                                    delegateQueue: nil];
+    }
+    
+    return self;
+}
+
+#pragma mark - Requests
+
+
+- (NSURL *) URL:(NSString *) URLString serializeParameters:(NSDictionary *) parameters {
+    
+    NSMutableArray *parametersList = [NSMutableArray new];
+    [parameters enumerateKeysAndObjectsUsingBlock:^(NSString *key, id obj, BOOL *stop) {
+        NSString *pair = [NSString stringWithFormat:@"%@=%@", key, obj];
+        [parametersList addObject: pair];
+    }];
+    
+    NSString *query = [parametersList componentsJoinedByString:@"&"];
+    if (query.length > 0)
+        query = [NSString stringWithFormat:@"?%@", query];
+    
+    NSURL *fullURL = [NSURL URLWithString: [URLString stringByAppendingString:query]
+                            relativeToURL: self.baseURL];
+    
+    return fullURL;
+}
+
+- (void) GET:(NSString *) URLString parameters:(NSDictionary *) parameters complete:(BaseServiceResponseBlock) complete {
+
+    NSURL *URL = [self URL: URLString
+       serializeParameters: parameters];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL: URL];
+    
+    [[self.baseSession dataTaskWithRequest: request
+                         completionHandler: ^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+                             
+                             NSError *jsonError;
+                             NSJSONSerialization *jsonSerialization = [NSJSONSerialization JSONObjectWithData: data
+                                                                                                      options: NSJSONReadingAllowFragments
+                                                                                                        error: &jsonError];
+
+                             if (!error) {
+                                 if (complete)
+                                     complete(response, jsonSerialization, error);
+                             }
+                             else {
+                                 NSHTTPURLResponse *urlResponse = (NSHTTPURLResponse *)response;
+                                 [self treatErrorWithOperation: urlResponse
+                                                         error: error
+                                                    completion: complete];
+                             }
+                         }] resume];
+}
+
+
+#pragma mark - Treatment Methods
+
+- (void) treatErrorWithOperation: (NSHTTPURLResponse *)         response
+                           error: (NSError *)                   operationError
+                      completion: (BaseServiceResponseBlock)    complete {
+    
+    NSString *commonErrorDescription = @"";
+    
+    // http response status code 401 (access denied)
+    if (response.statusCode == 401)
+        commonErrorDescription = @"Acesso negado.\nPor favor, autentique-se novamente!";
+    
+    // request timed out
+    else if ([operationError code] == NSURLErrorTimedOut)
+        commonErrorDescription = @"O tempo limite de conexão esgotou.\nPor favor, verifique sua conexão.";
+    
+    // undefined errors
+    else
+        commonErrorDescription = operationError.localizedDescription;
+    
+    
+    NSError *error = [NSError errorWithDomain: [response.URL host]
+                                         code: response.statusCode
+                                     userInfo: @{ NSLocalizedDescriptionKey: commonErrorDescription }];
+    
+    if (complete)
+        complete(response, nil, error);
+}
+
 
 @end
