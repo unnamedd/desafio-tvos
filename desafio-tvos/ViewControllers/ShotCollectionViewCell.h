ae8d2fb06a7e39d2fa0a7dbbccaffe97db50bbcd
Thiago Holanda <thiago.holanda@concretesolutions.com.br>
2015-09-28 16:27:23 -0300

    adicionado a classe da collectionview de shots

---
+++ desafio-tvos/ViewControllers/ShotCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ShotCollectionViewCell.h
+//  desafio-tvos
+//
+//  Created by Thiago Holanda on 9/25/15.
+//  Copyright © 2015 Concrete Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShotCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ desafio-tvos/ViewControllers/ShotCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  ShotCollectionViewCell.m
+//  desafio-tvos
+//
+//  Created by Thiago Holanda on 9/25/15.
+//  Copyright © 2015 Concrete Solutions. All rights reserved.
+//
+
+#import "ShotCollectionViewCell.h"
+
+@implementation ShotCollectionViewCell
+
+@end
