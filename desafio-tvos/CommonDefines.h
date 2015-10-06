f798fc8054c257c9a5622df91f6104e2e4e7ad68
Thiago Holanda <thiago.holanda@concretesolutions.com.br>
2015-10-06 17:18:22 -0300

    alterado o protocolo da URL da API do dribbble

--- desafio-tvos/CommonDefines.h
+++ desafio-tvos/CommonDefines.h
@@ -9,7 +9,7 @@
 #ifndef CommonDefines_h
 #define CommonDefines_h
 
-#define URL_BASE @"http://api.dribbble.com"
+#define URL_BASE @"https://api.dribbble.com"
 #define ACCESS_TOKEN @"49f95ae388d193249c045f4588576564131b8560a38a020b8538b5982ce23d5a"
 
 #endif /* CommonDefines_h */
